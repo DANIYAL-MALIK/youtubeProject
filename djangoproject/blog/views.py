23b5162d15b763c47e87beead8ddb70c11244f71
Daniyal Malik <myEmail@example.com>
2022-02-03 22:01:03 +0500

    Complete the templates successfully

--- djangoproject/blog/views.py
+++ djangoproject/blog/views.py
@@ -1,7 +1,24 @@
 from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
+posts=[
+    {
+        'author':'Daniyal Malik',
+        'title': 'I Love Django',
+        'content':'First post',
+        'date_posted':'August 19 2021'
+    },
+    {
+        'author':'Haji Abdul-Rehman',
+        'title': 'I Love Machine Learning',
+        'content':'Second Post post',
+        'date_posted':'August 14 2021'
+    }
+]
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context={
+        'posts':posts
+    }
+    return render(request, 'blog/home.html',context)
 def about(request):
-    return HttpResponse('<h1>You are on about page</h1>')
+    return render(request, 'blog/about.html',{'title':'About'})
