9eb03ab10a66777e87010415977e70c0e62e8ddc
Daniyal Malik <myEmail@example.com>
2022-02-04 17:26:29 +0500

    Complete the signup page successfully

--- djangoproject/blog/views.py
+++ djangoproject/blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
+from .models import Post
 posts=[
     {
         'author':'Daniyal Malik',
@@ -17,7 +18,7 @@ posts=[
 ]
 def home(request):
     context={
-        'posts':posts
+        'posts':Post.objects.all()
     }
     return render(request, 'blog/home.html',context)
 def about(request):
