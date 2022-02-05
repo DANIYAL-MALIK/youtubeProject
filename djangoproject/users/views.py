87391e217810ee1b321453b425af10621dc1fafa
Daniyal Malik <myEmail@example.com>
2022-02-05 15:59:56 +0500

    Profile of user is working

--- djangoproject/users/views.py
+++ djangoproject/users/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from .forms import UserRegistrationForm
 # Create your views here.
 def register(request):
     if request.method == 'POST':
@@ -9,9 +11,10 @@ def register(request):
             form.save()
             username=form.cleaned_data.get('username')
             messages.success(request,f'Account Created for the {username}!')
-            return redirect('blog-home')
+            return redirect('login')
     else: 
-        form=UserCreationForm()
+        form=UserRegistrationForm()
     return render (request,'users/register.html',{'form':form})
-    
-
+@login_required
+def profile(request):
+    return render(request,'users/profile.html')
