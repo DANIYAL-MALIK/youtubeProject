a04e6d5e54e8aa49c61daaf56960d16f9bd7c4cc
Daniyal Malik <myEmail@example.com>
2022-02-15 12:07:23 +0500

    Pagination is working successfully

--- djangoproject/blog/urls.py
+++ djangoproject/blog/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
-from .views import PositiveListView,PostDetailView,PostCreateview,PostUpdateview,PostDeleteView
+from .views import PositiveListView,UserPositiveListView,PostDetailView,PostCreateview,PostUpdateview,PostDeleteView
 urlpatterns = [
     path("",PositiveListView.as_view(), name="blog-home"),
+    path("user/<str:username>",UserPositiveListView.as_view(), name="user-posts"),
     path("about/",views.about, name="blog-about"),
     path("post/<int:pk>/delete/", PostDeleteView.as_view(), name="post-delete"),
     path("post/new",PostCreateview.as_view(), name="post-create"),
